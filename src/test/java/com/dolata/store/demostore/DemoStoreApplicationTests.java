d80c3d1780690e9e26599c65260ad9d0c54829ae
mariusz-repo-at-078400951273 <mariusz.dolata@capgemini.com>
2021-01-29 11:07:50 +0100

    init commit

---
+++ src/test/java/com/dolata/store/demostore/DemoStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolata.store.demostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
